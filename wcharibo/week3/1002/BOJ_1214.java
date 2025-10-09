22fd616c7440b1d278f69d081777a8ac0b13a186
wcharibo <woonggi4283@gmail.com>
2025-10-09 17:00:05 +0900

    [#1214/wcharibo] 쿨한 물건 구매

---
+++ wcharibo/week3/1002/BOJ_1214.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int D = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		int Q = Integer.parseInt(st.nextToken());
+		int result = Integer.MAX_VALUE;
+		
+		if(D % P == 0 || D % Q == 0) {
+			System.out.println(D);
+			return;
+		}
+		
+		int temp = Math.min(P, Q);
+		Q = Math.max(P, Q);
+		P = temp;
+		
+		int compensation = Q/P;
+		
+		int curP = D/P + 1;
+		int curQ = 0;
+		int cur = curP * P + curQ * Q;
+		
+		while(curP >= 0 && curQ >= 0) {
+			result = Math.min(result , cur);
+            
+			if(cur == D) {
+				break;
+			}
+			
+			curQ++;
+			curP-=compensation;
+            
+			while(curP * P + curQ * Q >= D) {
+				curP--;
+			}
+
+			curP++;
+			
+			cur = curP * P + curQ * Q;
+		}
+		System.out.println(result);
+	}
+}
